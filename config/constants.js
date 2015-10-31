e923894dd0e0558e6e5b5c6f978a600d2ba99621
Pascal <pascal.filippi@gmail.com>
2015-10-31 01:23:49 +0100

    Added role to JWT Added authentication check to route mechanism

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  roles: {
+      USER: "user",
+      RESCUER: "rescuer",
+      ADMIN: "admin"
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,35 @@ var express = require('express');
 var router = express.Router();
 var home = require('./home');
 var auth = require('./auth');
+var jwt = require('jsonwebtoken');
+var config = require('../config');
 
 var routes = [
     {"path": "/",           "method": "get",    "controller": home.index, "public": true},
     {"path": "/home",       "method": "get",    "controller": home.home, "public": true},
     {"path": "/login",      "method": "post",   "controller": auth.login, "public": true},
-    {"path": "/register",   "method": "post",   "controller": auth.register, "public": true}
+    {"path": "/register",   "method": "post",   "controller": auth.register, "public": true},
+    {"path": "/home_fail",  "method": "get",    "controller": home.home, "public": false}
 ];
 
 routes.forEach(function(item) {
     router[item.method](item.path, [
         function(req, res, next) {
             if(item.public) {
-                next();
+                return next();
             } else {
-                // jwt verification here can simply be extended to a role based scheme
+                try {
+                    var token = jwt.verify(req.get('Authorization'), config.SECRET);
+                    if(item.roles && item.roles.indexOf(token.role) < 0) {
+                        // User doesn't have required role
+                        return next("unauthorized");
+                    }
+                    return next();
+                } catch(e) {
+                    return next("unauthorized"); // TODO Add proper exceptions and error handling (http status codes)
+                }
             }
+            // We should never reach this point
         },
         item.controller
     ]);
